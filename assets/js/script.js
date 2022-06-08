527a31d4baabfb5b650ad03bff0190c726474f0a
Breno Vital <brenovital50@gmail.com>
2022-06-08 10:51:10 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ var cronometro = setInterval(function () {
   if (tempo < 0) {
     clearInterval(cronometro)
     clearInterval(criaCarol)
-    window.location.href = '../vitoria.html'
+    window.location.href = '/vitoria.html'
   } else
     document.getElementById('cronometro').innerHTML = tempo
 }, 1000)
@@ -47,7 +47,7 @@ function posicaoRandomica() {
     document.getElementById('carol').remove()
 
     if (vidas > 3) {
-      window.location.href = '../game-over.html'
+      window.location.href = '/game-over.html'
     } else {
       document.getElementById('v' + vidas).src = '../assets/images/passaporte-vazio.png';
 
@@ -66,7 +66,7 @@ function posicaoRandomica() {
 
   // elemento HTML
   var carol = document.createElement('img')
-  carol.src = '../assets/Images/carol.jpeg'
+  carol.src = './carol.jpeg'
   carol.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
   carol.style.left = posicaoX + 'px'
   carol.style.top = posicaoY + 'px'

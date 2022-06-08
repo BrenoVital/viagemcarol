7415147bcfc21ddf8a39727b3064c4825d7ed90a
Breno Vital <brenovital50@gmail.com>
2022-06-08 09:41:06 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+var altura = 0
+var largura = 0
+var vidas = 1
+var tempo = 3
+
+var criaCarolTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal') {
+  //1500
+  criaCarolTempo = 1500
+} else if (nivel === 'dificil') {
+  //1000
+  criaCarolTempo = 1000
+
+} else if (nivel === 'chucknorris') {
+  //750
+  criaCarolTempo = 750
+}
+
+function ajusteTamanhoJogo() {
+  altura = window.innerHeight
+  largura = window.innerWidth
+
+  console.log(altura, largura)
+}
+
+ajusteTamanhoJogo()
+
+
+var cronometro = setInterval(function () {
+  tempo -= 1
+  if (tempo < 0) {
+    clearInterval(cronometro)
+    clearInterval(criaCarol)
+    window.location.href = '/vitoria.html'
+  } else
+    document.getElementById('cronometro').innerHTML = tempo
+}, 1000)
+
+function posicaoRandomica() {
+
+  // remover a imagem anteior (caso exista)
+  if (document.getElementById('carol')) {
+    document.getElementById('carol').remove()
+
+    if (vidas > 3) {
+      window.location.href = '/game-over.html'
+    } else {
+      document.getElementById('v' + vidas).src = '/passaporte-vazio.png';
+
+      vidas++
+    }
+
+  }
+
+  var posicaoX = Math.floor(Math.random() * largura) - 90
+  var posicaoY = Math.floor(Math.random() * altura) - 90
+
+  posicaoX = posicaoX < 0 ? 0 : posicaoX
+  posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+  console.log(posicaoX, posicaoY)
+
+  // elemento HTML
+  var carol = document.createElement('img')
+  carol.src = '/carol.jpeg'
+  carol.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+  carol.style.left = posicaoX + 'px'
+  carol.style.top = posicaoY + 'px'
+  carol.style.position = 'absolute'
+  carol.id = 'carol'
+  carol.onclick = function () {
+    this.remove()
+  }
+
+  document.body.appendChild(carol)
+  ladoAleatorio()
+}
+
+function tamanhoAleatorio() {
+  var classe = Math.floor(Math.random() * 3)
+
+  switch (classe) {
+    case 0:
+      return 'carol'
+    case 1:
+      return 'carol-1'
+    case 2:
+      return 'carol-2'
+  }
+}
+
+function ladoAleatorio() {
+  var classe = Math.floor(Math.random() * 2)
+
+  switch (classe) {
+    case 0:
+      return 'ladoA'
+    case 1:
+      return 'ladoB'
+
+  }
+}
